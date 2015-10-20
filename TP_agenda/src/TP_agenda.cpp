f3f33f26d33a14942a771f6281e054497ee03834
AlexGounin <alexandregounin@aol.com>
2015-10-20 07:26:28 +0200

    Creation du projet TP agenda

---
+++ TP_agenda/src/TP_agenda.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TP_agenda.cpp
+// Author      : Alexandre Gounin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : TP Agenda
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
